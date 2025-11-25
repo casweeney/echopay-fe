c8c5595fb31b22f4373fa75ff49331c352dc0b08
markdavid000 <davidojukwu59@gmail.com>
2025-11-25 13:14:19 +0100

    feat: resolve bank accout details endpoit integrated successfully

--- redux/features/bank/bankAPI.ts
+++ redux/features/bank/bankAPI.ts
@@ -1,9 +1,22 @@
 import axiosClient from "@/lib/axiosClient";
-import { BanksResponse } from "@/types/bank";
+import {
+  BankDetailsPayload,
+  BankDetailsResponse,
+  BanksResponse,
+} from "@/types/bank";
 
 export const getBanks = async (): Promise<BanksResponse> => {
-  const { data } = await axiosClient.get<BanksResponse>(
-    "/api/v1/banks"
+  const { data } = await axiosClient.get<BanksResponse>("/api/v1/banks");
+  return data;
+};
+
+export const getBankDetails = async (
+  payload: BankDetailsPayload
+): Promise<BankDetailsResponse> => {
+  const { data } = await axiosClient.post<BankDetailsResponse>(
+    "/api/v1/resolve-account",
+    payload
   );
+
   return data;
 };
--- redux/features/bank/bankSlice.ts
+++ redux/features/bank/bankSlice.ts
@@ -1,16 +1,24 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { getBanks } from "./bankAPI";
-import { BanksData } from "@/types/bank";
+import { getBankDetails, getBanks } from "./bankAPI";
+import {
+  BankDetailsPayload,
+  BankDetailsResponse,
+  BanksData,
+} from "@/types/bank";
 
 export interface BankState {
   banks: BanksData | null;
+  bankDetails: BankDetailsResponse | null;
   loading: boolean;
+  fetchingDetails: boolean;
   error: string | null;
 }
 
 export const initialBankState: BankState = {
   banks: null,
+  bankDetails: null,
   loading: false,
+  fetchingDetails: false,
   error: null,
 };
 
@@ -19,6 +27,18 @@ export const fetchBanks = createAsyncThunk(
   async (_, { rejectWithValue }) => {
     try {
       const response = await getBanks();
+      return response;
+    } catch (error: any) {
+      return rejectWithValue(error?.response.data.message);
+    }
+  }
+);
+
+export const fetchBankDetails = createAsyncThunk(
+  "bank/fetchBankDetails",
+  async (payload: BankDetailsPayload, { rejectWithValue }) => {
+    try {
+      const response = await getBankDetails(payload);
       return response;
     } catch (error: any) {
       return rejectWithValue(error?.response.data.message);
@@ -43,6 +63,18 @@ const bankSlice = createSlice({
       .addCase(fetchBanks.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload as string;
+      })
+      .addCase(fetchBankDetails.pending, (state) => {
+        state.fetchingDetails = true;
+      })
+      .addCase(fetchBankDetails.fulfilled, (state, action) => {
+        state.fetchingDetails = false;
+        state.bankDetails = action.payload;
+      })
+      .addCase(fetchBankDetails.rejected, (state, action) => {
+        state.fetchingDetails = false;
+        state.bankDetails = null;
+        state.error = action.error as string;
       });
   },
 });
